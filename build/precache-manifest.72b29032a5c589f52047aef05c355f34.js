cb8fbbfcc9ed2b5c51066e86ea9c4ec709ff2f1e
brontto <bronttosaur@gmail.com>
2020-07-07 18:51:09 +0300

    pientä säätöö

---
+++ build/precache-manifest.72b29032a5c589f52047aef05c355f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a713f7bcf94fe05b4440aef11ee5d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59dc3966ea93e6ea10d4",
+    "url": "/static/css/main.850506f8.chunk.css"
+  },
+  {
+    "revision": "ea7dfe846377567f1a2f",
+    "url": "/static/js/2.2de4c12b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2de4c12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59dc3966ea93e6ea10d4",
+    "url": "/static/js/main.35a9c75f.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
